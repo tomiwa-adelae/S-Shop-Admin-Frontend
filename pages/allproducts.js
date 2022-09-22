cb827424280d895b3a99ac4a83bc9b30c7fbe8e1
thetommedia@gmail.com <thetommedia@gmail.com>
2022-09-22 17:03:07 +0100

    Added

--- pages/allproducts.js
+++ pages/allproducts.js
@@ -1,13 +1,13 @@
 import BackBtn from '../components/BackBtn';
-import AllProducts from '../components/AllProductspageComponents/AllProducts';
-import AllProductsIntro from '../components/AllProductspageComponents/AllProductsIntro';
+// import AllProducts from '../components/AllProductspageComponents/AllProducts';
+// import AllProductsIntro from '../components/AllProductspageComponents/AllProductsIntro';
 
 const allproducts = () => {
    return (
       <div className="all-products-page">
          <BackBtn to="/dashboard" />
-         <AllProductsIntro />
-         <AllProducts />
+         {/* <AllProductsIntro />
+         <AllProducts /> */}
       </div>
    );
 };
